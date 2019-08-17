18e601cc00b10e2852269b4ccceada2fc7d66059
shalini-polapally <polapallyshalini1@gmail.com>
2019-08-17 20:02:32 +0530

    Login form with client-side validation

---
+++ src/components/messages/InlineError.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InlineError = ({ text }) => (
+    <span style={{ color: "#912d2b" }}>
+        {text}
+    </span >);
+
+InlineError.propTypes = {
+    text: PropTypes.string.isRequired
+};
+
+export default InlineError;
