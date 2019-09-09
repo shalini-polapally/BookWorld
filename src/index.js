df79fe29e352404d6fff1d913d9e640e634a4518
shalini-polapally <polapallyshalini1@gmail.com>
2019-09-09 15:10:15 +0530

    login and logout

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ import thunk from 'redux-thunk';
 import App from './App';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "./rootReducer";
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk))
 
 );
+if (localStorage.bookworldJWT) {
+    const user = { token: localStorage.bookworldJWT };
+    store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(<BrowserRouter><Provider store={store} ><App /></Provider></BrowserRouter>, document.getElementById('root'))
 
